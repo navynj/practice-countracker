dae630fd097e5b17e96ddd365198a6bd5347dfb1
Yoonji <navyoonj@gmail.com>
2025-08-12 17:45:31 -0700

    Feat: Setup | DB & Auth (#2)

---
+++ app/api/project/route.ts
@@ -0,0 +1,51 @@
+import { authOptions } from '@/lib/auth';
+import { prisma } from '@/prisma/client';
+import { getServerSession } from 'next-auth';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(req: NextRequest) {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return new Response('Session not found', {
+      status: 401,
+    });
+  }
+
+  try {
+    const data = await prisma.project.findMany({
+      where: { userId: session.user.id },
+      orderBy: [{ createdAt: 'asc' }],
+    });
+
+    return new Response(JSON.stringify(data), { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return new Response('Failed to fetch categories', { status: 500 });
+  }
+}
+
+export async function POST(req: Request) {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return new Response('Session not found', {
+      status: 401,
+    });
+  }
+
+  const reqData = await req.json();
+  try {
+    const data = await prisma.project.create({
+      data: {
+        ...reqData,
+        userId: session.user.id,
+      },
+    });
+
+    return new Response(JSON.stringify(data), { status: 201 });
+  } catch (error) {
+    console.error(error);
+    return new Response('Failed to create project', { status: 500 });
+  }
+}
